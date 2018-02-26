cbd39657150707a7e135a9f61735b4bc244fcf27
syahrulromansah <35859980+syahrulromansah@users.noreply.github.com>
2018-02-26 16:19:25 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# test-bot
-Line Bot
+# DONE UPLODE 26_FEBRUARI_2018
+
+# BOT AUTO LIKE
+V2.1 editor_::
+SyahruelBot
+# BOT AUTO COMMAND TIMELINE
+V2.1 last update::
+26/02/2018
+# CONTACT OFFICIAL
+
+<a href="http://line.me/ti/p/%40xdp6706b"><img height="36" border="0" alt="Add Friend" src="https://scdn.line-apps.com/n/line_add_friends/btn/en.png"></a>
+
+# LINE ME
+
+[ADD_LINE](http://line.me/ti/p/~ruel2021)
+
+
+THANKS TO.
+allah swt yang maha agung
+syahruel
+¤??ÂMVÂXBØ?¤
+and friend theme
---
+++ ruellikebot.py
@@ -0,0 +1,231 @@
+# -*- coding: utf-8 -*-
+
+import RUEL
+from RUEL.lib.curve.ttypes import *
+from datetime import datetime
+import time,random,sys,json,codecs,threading,glob,sys
+import re,string,os
+import os.path,sys,urllib,shutil,subprocess
+
+
+cl = RUEL.LINE()
+cl.login(token='Eq04DpsMLfu02jDW0nl1.44BiFlZscfloFrkOwDOFWq.4vH4tE2oemAKww1VvFlxjJEI9UHCQR/FX74S6x/ZueI=')
+cl.loginResult()
+
+print u"===========RUEL SUKSES LOGIN BOT LIKE==================="
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+KAC = [cl]
+mid = cl.getProfile().mid
+
+Bots = [mid]
+admid= "ue386c87cbcd20852ce7d12a859da1fc1"
+wait = {
+    'contact':False,
+    'autoJoin':True,
+    'autoCancel':{"on":True,"members":1},
+    'leaveRoom':True,
+    'timeline':True,
+    'autoAdd':False,
+    'message':"☆••••••••AUTO ADD••••••••☆\nhttp://line.me/ti/p/%40xdp6706b\n『⊰์◉⊱ᎢᎬᎪᎷ ᏴᏞᎪᏟᏦ Ruel ᏀᎪᎷᎬᎡ⊰์◉⊱』",
+    "lang":"JP",
+    "comment1":"☆••••••••••••••••••••••••••☆\nhttp://line.me/ti/p/%40xdp6706b\n『⊰์◉⊱ᎢᎬᎪᎷ ᏴᏞᎪᏟᏦ Ruel ᏀᎪᎷᎬᎡ⊰์◉⊱』",
+    "commentOn":False,
+    "commentBlack":{},
+    "wblack":False,
+    "dblack":False,
+    "clock":False,
+    "posts":True,
+	}
+	
+wait2 = {
+	'readMember':{},
+	'readPoint':{},
+	'ROM':{},
+	'setTime':{}
+    }
+	
+setTime = {}
+setTime = wait2["setTime"]
+
+res = {
+    'num':{},
+    'us':{},
+    'au':{},
+}
+
+
+def Cmd(string, commands): #/XXX, >XXX, ;XXX, ^XXX, %XXX, $XXX...
+    tex = [""]
+    for texX in tex:
+        for command in commands:
+            if string ==texX + command:
+                return True
+    return False
+
+def sendMessage(to, text, contentMetadata={}, contentType=0):
+    mes = Message()
+    mes.to, mes.from_ = to, profile.mid
+    mes.text = text
+    mes.contentType, mes.contentMetadata = contentType, contentMetadata
+    if to not in messageReq:
+        messageReq[to] = -1
+    messageReq[to] += 1
+    
+    
+def bot(op):
+    try:
+        if op.type == 0:
+            return
+        if op.type == 5:
+            if wait["autoAdd"] == True:
+                cl.findAndAddContactsByMid(op.param1)
+                if (wait["message"] in [""," ","\n",None]):
+                    pass
+                else:
+                    cl.sendText(op.param1,str(wait["message"]))
+     	if op.type == 26:
+            msg = op.message
+            if msg.toType == 0:
+                msg.to = msg.from_
+                if msg.from_ == admin:
+                    if "join:" in msg.text:
+                        list_ = msg.text.split(":")
+                        try:
+                            cl.acceptGroupInvitationByTicket(list_[1],list_[2])
+                            X = cl.getGroup(list_[1])
+                            X.preventJoinByTicket = True
+                            cl.updateGroup(X)
+                        except:
+                            cl.sendText(msg.to,"error")
+        if msg.contentType == 16:
+            url = msg.contentMetadata["postEndUrl"]
+            cl.like(url[25:58], url[66:], likeType=1001)
+            cl.comment(url[25:58], url[66:], wait["comment1"])
+        if op.type == 26:
+            msg = op.message
+            if msg.toType == 0:
+                msg.to = msg.from_
+                if msg.from_ == admin:
+                    if "join:" in msg.text:
+                        list_ = msg.text.split(":")
+                        try:
+                            cl.acceptGroupInvitationByTicket(list_[1],list_[2])
+                            X = cl.getGroup(list_[1])
+                            X.preventJoinByTicket = True
+                            cl.updateGroup(X)
+                        except:
+                            cl.sendText(msg.to,"error")                    
+            
+            elif msg.contentType == 16:
+                if wait["timeline"] == True:
+                    msg.contentType = 0
+                    if wait["lang"] == "JP":
+                        msg.text = "menempatkan URL\n" + msg.contentMetadata["postEndUrl"]
+                    else:
+                        msg.text = "URLâ†’\n" + msg.contentMetadata["postEndUrl"]
+                    cl.sendText(msg.to,msg.text)
+            elif msg.text is None:
+                return
+#--------------------Fungsi Tag All Finish--------------------#
+            elif "Mid" == msg.text:
+                cl.sendText(msg.to)
+#-------------------------------------------------------------------#
+            elif msg.text in ["愛のプレゼント","Gift"]:
+                msg.contentType = 9
+                msg.contentMetadata={'PRDID': 'a0768339-c2d3-4189-9653-2909e9bb6f58',
+                                    'PRDTYPE': 'THEME',
+                                    'MSGTPL': '5'}
+                                    
+            elif "Me" == msg.text:
+                msg.contentType = 13
+                msg.contentMetadata = {'mid': msg.to}
+                cl.sendMessage(msg)
+		
+            elif "Tagall" in msg.text:
+                group = cl.getGroup(msg.to)
+                k = len(group.members)//100
+                for j in xrange(k+1):
+                    msg = Message(to=msg.to)
+                    txt = u''
+                    s=0
+                    d=[]
+                    for i in group.members[j*100 : (j+1)*100]:
+                        d.append({"S":str(s), "E" :str(s+8), "M":i.mid})
+                        s += 9
+                        txt += u'@Krampus\n'
+                    msg.text = txt
+                    msg.contentMetadata = {u'MENTION':json.dumps({"MENTIONEES":d})}
+                    cl.sendMessage(msg)             
+    
+            elif "Cek" in msg.text:
+				cl.sendText(msg.to, "siders on")
+				try:
+					del wait2['readPoint'][msg.to]
+					del wait2['readMember'][msg.to]
+				except:
+					pass
+				wait2['readPoint'][msg.to] = msg.id
+				wait2['readMember'][msg.to] = ""
+				wait2['setTime'][msg.to] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+				wait2['ROM'][msg.to] = {}
+				print wait2
+            elif "Cctv" in msg.text:
+				if msg.to in wait2['readPoint']:
+					if wait2["ROM"][msg.to].items() == []:
+						chiya = ""
+					else:
+						chiya = ""
+						for rom in wait2["ROM"][msg.to].items():
+							print rom
+							chiya += rom[1] + "\n"
+
+					cl.sendText(msg.to, "people who reading%s\n is this\n\n\nDate and time I started it:\n[%s]" % (wait2['readMember'][msg.to],setTime[msg.to]))
+				else:
+					cl.sendText(msg.to, "Ketik dulu cek\nBaru ketik cctv dasar peak")
+
+#-----------------------------------------------------------
+        if op.type == 59:
+            print op
+
+
+    except Exception as error:
+        print error
+
+
+def a2():
+    now2 = datetime.now()
+    nowT = datetime.strftime(now2,"%M")
+    if nowT[14:] in ["10","20","30","40","50","00"]:
+        return False
+    else:
+        return True
+def nameUpdate():
+    while True:
+        try:
+        #while a2():
+            pass
+            if wait["clock"] == True:
+                now2 = datetime.now()
+                nowT = datetime.strftime(now2,"(%H:%M)")
+                profile = cl.getProfile()
+                profile.displayName = wait["cName"] + nowT
+                cl.updateProfile(profile)
+            time.sleep(600)
+        except:
+            pass
+thread2 = threading.Thread(target=nameUpdate)
+thread2.daemon = True
+thread2.start()
+
+while True:
+    try:
+        Ops = cl.fetchOps(cl.Poll.rev, 5)
+    except EOFError:
+        raise Exception("It might be wrong revision\n" + str(cl.Poll.rev))
+
+    for Op in Ops:
+        if (Op.type != OpType.END_OF_OPERATION):
+            cl.Poll.rev = max(cl.Poll.rev, Op.revision)
+            bot(Op)
